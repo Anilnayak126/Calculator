2cc0b264b34b59eb4e13aa60775b1c75d1ac0baf
Anil Nayak <nayakanil43603@gmail.com>
2024-08-05 01:26:42 +0530

    adding delete botton

--- Src.js
+++ Src.js
@@ -9,6 +9,13 @@ function clearDisplay(){
     display.value = '';
 }
 
+function Delete(){
+    // OnClick="Calc.Input.value = Calc.Input.value.substring(0, Calc.Input.value.length - 1)"
+     let delete_nums =  display.value.substring(0, display.value.length -1)
+
+    display.value = delete_nums
+}
+
 function calculate(){
     try{
 
